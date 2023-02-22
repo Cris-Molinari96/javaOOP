148fe8d5d271f84b16a882bbb1e6ccf45e3fc406
Cristian Molinari <cris.molinari96@gmail.com>
2023-02-22 09:03:17 +0100

    add pacÃk 4

--- _1/Course.java
+++ _1/Course.java
@@ -3,9 +3,7 @@ package javaOOP._1;
 public class Course {
 
     String courseName ;
-    String positionStudent;
     int maxStudents;
     byte qualityRatingOutOf10;
-    byte ageStudent;
-    String students;
+
 }
