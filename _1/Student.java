8d8594c1d04f26782f778d3713508bbec6c7c2d2
Cristian Molinari <cris.molinari96@gmail.com>
2023-02-27 20:25:36 +0100

    modified pack 1

--- _1/Student.java
+++ _1/Student.java
@@ -1,10 +1,13 @@
 package javaOOP._1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
 String name ;
 String surname ;
 int age ;
-
+List students = new ArrayList<>();
 //
     @Override
     public String toString() {
